10f326778eb1a3f3efcf7b54a8c6650467b1977b
caohuan95568 <602269452@qq.com>
2017-11-23 23:18:23 +0800

    add

---
+++ WebApplication1/WebApplication1/DAL/DBContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace WebApplication1.DAL
+{
+    public class DBContextFactory
+    {
+        //线程槽
+        public static DbContext CreateDbContext()
+        {
+            DbContext dbContext = (DbContext)CallContext.GetData("dbContext");
+            if (dbContext == null)
+            {
+                dbContext = new OAEntities();
+                CallContext.SetData("dbContext", dbContext);
+            }
+            return dbContext;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/IDAL/IDBSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.IDAL
+{
+    
+        public partial interface IDBSession
+        {
+            DbContext Db { get; }
+            // IUserInfoDal UserInfoDal { get; set; }
+            bool SaveChanges();
+            int ExecuteSql(string sql, params SqlParameter[] pars);
+            List<T> ExecuteQuery<T>(string sql, params SqlParameter[] pars);
+        }
+  
+}
